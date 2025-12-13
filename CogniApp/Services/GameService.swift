4060d47911338480f0f23c3657fc6dfbd08bf673
Odei Garcia <odei0209@gmail.com>
2025-12-13 17:18:27 +0100

    firebase backend

---
+++ CogniApp/Services/GameService.swift
@@ -0,0 +1,32 @@
+import FirebaseFirestore
+
+class GameService {
+    private let db = Firestore.firestore()
+
+    func saveGameResult(_ result: GameResult, completion: @escaping (Result<Void, Error>) -> Void) {
+        do {
+            let _ = try db.collection("gameResults").addDocument(from: result) { error in
+                if let error = error {
+                    completion(.failure(error))
+                } else {
+                    completion(.success(()))
+                }
+            }
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+
+    func fetchGameResults(forUserId userId: String, completion: @escaping (Result<[GameResult], Error>) -> Void) {
+        db.collection("gameResults").whereField("userId", isEqualTo: userId).getDocuments { snapshot, error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                let results: [GameResult] = snapshot?.documents.compactMap { document in
+                    return try? document.data(as: GameResult.self)
+                } ?? []
+                completion(.success(results))
+            }
+        }
+    }
+}
