8234f006f2c0287133ae311cf96d5f071adbaa0f
Odei Garcia <odei0209@gmail.com>
2025-12-13 17:46:53 +0100

    millora models

---
+++ CogniApp/Models/CognitiveDomain.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum CognitiveDomain: String, Codable {
+    case attention
+    case speedProcessing
+    case verbalFluency
+    case memory
+    case executiveFunctions
+}
--- CogniApp/Models/User.swift
+++ CogniApp/Models/User.swift
@@ -1,6 +1,8 @@
 struct User: Identifiable, Codable {
     let id: String
     let name: String
+    let surname: String
+    let surname2: String?
     let email: String
     let age: Int?
 }
