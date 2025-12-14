2f54011aaaf901ab554dff63dd9bd66b49d17796
joan.solina <joan.solina@estudiantat.upc.edu>
2025-12-14 10:03:12 +0100

    Attention

--- CogniApp/ViewControllers/Games/AttentionVC.swift
+++ CogniApp/ViewControllers/Games/AttentionVC.swift
@@ -1,29 +1,143 @@
-//
-//  AttentionVC.swift
-//  CogniApp
-//
-//  Created by Dylan Bautista on 12/12/25.
-//
-
 import UIKit
 
 class AttentionVC: UIViewController {
-    
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-            super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-            // Your custom setup here
-    }
-    
-    required init?(coder: NSCoder) {
-            super.init(coder: coder)
-            // Or, if you don’t support storyboard initialization:
-            fatalError("init(coder:) has not been implemented")
-    }
+
+    // MARK: - Voice Recorder
+    let voiceRecorder = VoiceRecorder()
+
+    // MARK: - UI
+    private let sequenceLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 44, weight: .bold)
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+
+    // MARK: - Joc
+    private var currentLength = 4
+    private let maxLength = 9
+    private let seriesPerLength = 2
+    private var currentSeries = 0
+    private var currentSequence: [Int] = []
+    private var isGameOver = false
+
+    // MARK: - Temps
+    private let maxTimePerSequence: TimeInterval = 10
+    private var sequenceTimer: Timer?
+    private var elapsedTime: TimeInterval = 0
+
+    // Estadístiques
+    private var numErrors = 0
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupLabel()
+        startGame()
     }
-    
-    
-}
 
+    private func setupLabel() {
+        sequenceLabel.frame = view.bounds
+        sequenceLabel.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(sequenceLabel)
+    }
+
+    // MARK: - Joc
+    private func startGame() {
+        currentLength = 4
+        currentSeries = 0
+        numErrors = 0
+        isGameOver = false
+        nextRound()
+    }
+
+    private func nextRound() {
+        guard !isGameOver else { return }
+
+        if currentLength > maxLength {
+            finishGame()
+            return
+        }
+
+        if currentSeries >= seriesPerLength {
+            currentLength += 1
+            currentSeries = 0
+        }
+
+        generateSequence()
+        showSequence()
+        startListening()
+        startSequenceTimer()
+    }
+
+    private func generateSequence() {
+        currentSequence = (0..<currentLength).map {
+            Int.random(in: 0...9)
+        }
+    }
+
+    private func showSequence() {
+        sequenceLabel.text = currentSequence.map { "\($0)" }.joined(separator: " ")
+        elapsedTime = 0
+    }
+
+    // MARK: - Veu
+    private func startListening() {
+        DispatchQueue.global(qos: .userInitiated).async {
+            let spokenWords = self.voiceRecorder.startVoiceRecording(false)
+            let spokenNumbers = self.convertStringsToInts(spokenWords)
+
+            DispatchQueue.main.async {
+                self.sequenceTimer?.invalidate()
+                self.checkAnswer(userNumbers: spokenNumbers)
+            }
+        }
+    }
+
+    // MARK: - Temporitzador
+    private func startSequenceTimer() {
+        sequenceTimer?.invalidate()
+        sequenceTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            self.elapsedTime += 0.1
+            if self.elapsedTime >= self.maxTimePerSequence {
+                self.sequenceTimer?.invalidate()
+                self.numErrors += 1
+                self.nextRound()
+            }
+        }
+    }
+
+    // MARK: - Comprovació
+    private func checkAnswer(userNumbers: [Int]) {
+        if userNumbers == currentSequence {
+            currentSeries += 1
+        } else {
+            numErrors += 1
+        }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.nextRound()
+        }
+    }
+
+    // MARK: - Final del joc
+    private func finishGame() {
+        isGameOver = true
+        sequenceTimer?.invalidate()
+
+        let alert = UIAlertController(
+            title: "Joc finalitzat",
+            message: "Errors totals: \(numErrors)",
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
+    // MARK: - Utils
+    private func convertStringsToInts(_ strings: [String]) -> [Int] {
+        strings.compactMap { Int($0) }
+    }
+}
